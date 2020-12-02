a61b0f53a2897e6e589c55f26714ab3a90c71f15
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2020-12-02 14:04:55 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,4 +4,4 @@ set -e
 
 ./wait_for_it.sh ${OCLAPI_HOST}:${OCLAPI_PORT} -t 0
 
-java -Dhibernate.types.print.banner=false -jar ocl-fhir-ts/target/oclfhir.jar
+java -Xmx512m -Dhibernate.types.print.banner=false -jar ocl-fhir-ts/target/oclfhir.jar
