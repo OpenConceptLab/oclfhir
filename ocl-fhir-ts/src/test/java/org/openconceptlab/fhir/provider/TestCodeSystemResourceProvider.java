cb0279375d92abc5527a615ca740f0b50b228e69
Harsh Patel <hp11@regenstrief.org>
2021-04-12 12:13:57 -0400

    fix to unit test

--- ocl-fhir-ts/src/test/java/org/openconceptlab/fhir/provider/TestCodeSystemResourceProvider.java
+++ ocl-fhir-ts/src/test/java/org/openconceptlab/fhir/provider/TestCodeSystemResourceProvider.java
@@ -221,10 +221,10 @@ public class TestCodeSystemResourceProvider extends OclFhirTest {
         CodeSystemResourceProvider provider = codeSystemProvider();
         Bundle bundle = provider.searchCodeSystemByUrl(newString(URL_SOURCE_1), newString("*"), null, null, null, null, null, requestDetails);
         assertEquals(2, bundle.getTotal());
-        assertEquals(URL_SOURCE_1, ((CodeSystem) bundle.getEntry().get(0).getResource()).getUrl());
-        assertEquals(V_1_0, ((CodeSystem) bundle.getEntry().get(0).getResource()).getVersion());
         assertEquals(URL_SOURCE_1, ((CodeSystem) bundle.getEntry().get(1).getResource()).getUrl());
         assertEquals("v2.0", ((CodeSystem) bundle.getEntry().get(1).getResource()).getVersion());
+        assertEquals(URL_SOURCE_1, ((CodeSystem) bundle.getEntry().get(0).getResource()).getUrl());
+        assertEquals(V_1_0, ((CodeSystem) bundle.getEntry().get(0).getResource()).getVersion());
     }
 
     @Test
