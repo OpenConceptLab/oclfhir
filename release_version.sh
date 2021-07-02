a9844996d35714978f2a7b6767eab07e709af9c6
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2021-07-02 19:07:58 +0200

    Update release_version.sh

--- release_version.sh
+++ release_version.sh
@@ -15,8 +15,9 @@ export SOURCE_COMMIT=${SOURCE_COMMIT:0:8}
 GET_VERSION_CMD="$MAVEN_BIN -Dexec.executable='echo' -Dexec.args='\${project.version}' --non-recursive exec:exec -q"
 PROJECT_VERSION=$(eval "$GET_VERSION_CMD")
 
-TAG="$PROJECT_VERSION-$SOURCE_COMMIT"
+TAG="$PROJECT_VERSION"
 
+git checkout -b release
 git add .
 git commit -m "Set build version"
 
@@ -24,6 +25,7 @@ git tag "$TAG"
 
 git remote set-url origin ${GIT_REPO_URL}
 git push origin --tags
+git checkout master
 
 docker pull $DOCKER_IMAGE_ID
 docker tag $DOCKER_IMAGE_ID $DOCKER_IMAGE_NAME:$TAG
