92776059d7ee6f5c4fbc171d9121704365789eec
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2021-07-02 14:32:51 +0200

    Update set_build_version.sh

--- set_build_version.sh
+++ set_build_version.sh
@@ -2,10 +2,17 @@
 
 set -e
 
+MAVEN_BIN = "$MAVEN_HOME/bin/mvn"
+if [[ "$MAVEN_HOME" == "" ]]; then
+  echo "[INFO] MAVEN_HOME variable is unset. Trying to use 'mvn' from the system path."
+  MAVEN_BIN = "mvn"
+fi
+
 GIT_SHA=${SOURCE_COMMIT:-'SNAPSHOT'}
 GIT_SHA=${GIT_SHA:0:8}
 
-PROJECT_VERSION=$(mvn -Dexec.executable='echo' -Dexec.args='${project.version}' --non-recursive exec:exec -q)
+GET_VERSION_CMD="$MAVEN_BIN -Dexec.executable='echo' -Dexec.args='${project.version}' --non-recursive exec:exec -q"
+PROJECT_VERSION=$(eval "$GET_VERSION_CMD")
 
 PROJECT_MAINTENANCE_VERSION=$(echo "$PROJECT_VERSION" | cut -f 1 -d '-')
 
@@ -13,4 +20,4 @@ NEW_PROJECT_VERSION="$PROJECT_MAINTENANCE_VERSION-$GIT_SHA"
 
 echo "Setting project version to $NEW_PROJECT_VERSION"
 
-mvn versions:set -DnewVersion="$NEW_PROJECT_VERSION" -q
+$MAVEN_BIN versions:set -DnewVersion="$NEW_PROJECT_VERSION" -q
